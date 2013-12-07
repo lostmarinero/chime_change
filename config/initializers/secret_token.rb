179a08cb1153e9fdcc7a0b0faf228ed5ecdfd578
kbsail <berry.kevinw@gmail.com>
2013-12-06 22:19:44 -0800

    rails initiated

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChimeApp::Application.config.secret_token = '1686b73737f60eeeb93e3aaa51d138d211c952aeefe9438e774e11f451c1cb6d3b3ed700354032efc76e5c46c69a01097600f72eb8bbaa7f1ad9abe00c147ff3'
