c76c6640d0bac7e2ec41811f7ed9b3b1771d2f96
kbsail <berry.kevinw@gmail.com>
2013-12-07 13:23:44 -0800

    working on integrating javascript

--- app/assets/javascripts/maryam.js
+++ app/assets/javascripts/maryam.js
@@ -65,71 +65,47 @@ var o = {
 		}
 	},
 
-}
-
-$(function(){ o.init(); });
-
-var map, geocoder, marker, location,
-	ey, my, mouseDown = false;
-	
-var o = {
-	init: function(){
-		this.map.init();
-		
+  getChimes: function() {
+	  var chimeData = new Array();
+	  $.getJSON('/chimes/json', function(data) {
+	    for (i in data){
+	      var info = data[i];
+	      var chime = {
+	        created_at : info['chimes'][0]['created_at'],
+	        tweet_id : info['chimes'][0]['tweet_id'],
+	        favorite_count : info['chimes'][0]['favorite_count'],
+	        retweet_count : info['chimes'][0]['retweet_count'],
+	        img : info['chimes'][0]['user']['profile_image_url'],
+	        screen_name : info['chimes'][0]['user']['screen_name'],
+	        user_id : info['chimes'][0]['user']['id'],
+	        name : info['chimes'][0]['user']['name'],
+	        // tweet : info['chimes'][0]['tweet'],
+	        lat : info['chimes'][0]['geo']['coordinates'][0],
+	        lon : info['chimes'][0]['geo']['coordinates'][1],
+	        // place : info['chimes'][0]['place'],
+	        // tweet_url : info['url']
+	        // action : info['chimes'][0]['action']; 
+	      }
+	    chimeData.push(chime);
+	    }
+	  }
+	  return chimeData
 	},
-    map: {
-		size: function(){
-			var w = $(window).width(),
-				h = $(window).height();
-			return { width: w, height: h }
-		},
 
-		getData: function() {
-		  var center = new google.maps.LatLng(52, 52); //default
-		  navigator.geolocation.getCurrentPosition(function() {
-		     center = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-		   });
-		   return {
-				zoom: 3,
-				mapTypeId: google.maps.MapTypeId.ROADMAP,
-				center: center
-			};
-		},
-
-        // Drops a Pin
-	    createMarker: function(chime) {
-		  var marker =  new google.maps.Marker(
-		    {icon: chime.img, map: map, title: chime.screen_name, position: new google.maps.LatLng(chime.longitude, chime.latitude)});
-			
-		  google.maps.event.addListener(marker, 'click', function(){
-			  alert("hello world");
-			// this is where the click handler will go
-		  }); 
-		  return marker;
-		},
-		
-		show: function(){
-			var c = new Object();
-			c.screen_name = "Maryam";
-			c.img = "";
-			c.title="Hello World";
-			c.longitude=32.9310417;
-			c.latitude=-80.0364981;
-			this.createMarker(c);
-		},
-		init: function(){
-			var size = o.map.size();
-			$('#map').css({ width: size.width, height: size.height });
-			map = new google.maps.Map(document.getElementById('map'), o.map.getData());
-			geocoder = new google.maps.Geocoder();
-			o.map.show();
-			google.maps.event.addListener(map, 'dragstart', function(){
-				$('.posts').hide();
-			}); 
+	loadData: function() {
+		var all_chimes = o.map.getChimes();
+		for (i in all_chimes){
+			o.map.createMarker(i)
 		}
-	},
+	}
 
 }
 
-$(function(){ o.init(); });
 
+
+
+$(function(){ o.init(); 
+
+
+
+});
